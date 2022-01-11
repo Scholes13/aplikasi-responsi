f409325f2c7bfa3d9a8620101f9dacc10f5be683
Scholes13 <prammujiarif@gmail.com>
2022-01-11 15:32:22 +0700

    frgament

--- app/src/main/java/com/paymu/app/MainActivity.java
+++ app/src/main/java/com/paymu/app/MainActivity.java
@@ -1,14 +1,46 @@
 package com.paymu.app;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    private BottomNavigationView.OnNavigationItemSelectedListener navigation = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            Fragment f = null;
+            switch (item.getItemId()) {
+                case R.id.menu_home:
+                    f = new FragmentHome();
+                    break;
+                case R.id.menu_payment:
+                    f = new FragmentPayment();
+                    break;
+                case R.id.menu_history:
+                    f = new FragmentHistory();
+                    break;
+                case R.id.menu_setting:
+                    f = new FragmentSetting();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment, f).commit();
+            return true;
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_menu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigation);
     }
 }
