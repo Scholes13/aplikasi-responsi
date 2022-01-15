4e8ee5f028a97c4924e7dede5c3f00626b0829dd
scholes13 <prammujiarif@gmail.com>
2022-01-15 08:56:00 -0800

    profile, scan

--- app/src/main/java/com/paymu/app/FragmentSetting.java
+++ app/src/main/java/com/paymu/app/FragmentSetting.java
@@ -1,18 +1,34 @@
 package com.paymu.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FragmentSetting extends Fragment {
-    @Nullable
+import com.paymu.app.R;
+import com.paymu.app.profile;
+
+public class SettingsFragment extends Fragment {
+
+    ImageView profile;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_setting,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+
+        profile = view.findViewById(R.id.profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity().getApplication(), profile.class));
+            }
+        });
+        return view;
     }
 }
